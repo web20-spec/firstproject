08f342e7d54ee74bfbb4a1650522658491df69d9
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-10-23 11:55:09 -0700

    Added new responsive design

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const  [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/hiteshchoudhary')
-        .then(response => response.json())
-        .then(data => {
-            setData(data)
-        }
-        )
-    }, [])
-  return (
+    const data = useLoaderData()
+    // const  [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setData(data)
+    //     }
+    //     )
+    // }, [])
+return (
     <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>
         Github Followers: {data.followers} 
         <img src={data.avatar_url} alt="Git picture" width={250} />
         </div>
-  )
+)
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
